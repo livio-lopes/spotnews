746c6a6017e3cb2c8caff6987f2112aecc475371
Lívio Lopes <antonio.lvio760@gmail.com>
2023-12-05 16:42:26 -0300

    feat(views): create details_page for render news_details.html

--- news/views.py
+++ news/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render
-from news.models import News
+from news.models import News, Category
 
 
 # Create your views here.
 def index(request):
     context = {"title": "Página Inicial", "news": News.objects.all()}
     return render(request, "home.html", context)
+
+
+def details_page(request, id):
+    get_news = News.objects.get(id=id)
+    get_categories = Category.objects.get(id=get_news.id)
+    context = {
+        "title": "Página de Detalhes da Notícia",
+        "report": get_news,
+        "categories": get_categories,
+    }
+    return render(request, "news_details.html", context)
