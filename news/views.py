b892a4e8e52b5df562c71111801644c23f7a9510
Lívio Lopes <antonio.lvio760@gmail.com>
2023-12-05 15:05:28 -0300

    feat(urls.py): includes news/urls.py with routes on spotnews/urls

--- news/views.py
+++ news/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from news.models import News
+
 
 # Create your views here.
+def index(request):
+    context = {"title": "Página Inicial", "news": News.objects.all()}
+    return render(request, "home.html", context)
