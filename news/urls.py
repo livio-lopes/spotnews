c419e9ff84c053e6958dc6732831964e1457f38a
Lívio Lopes <antonio.lvio760@gmail.com>
2023-12-06 19:01:55 -0300

    feat(urls.py): create route for form with method POST on /categories

--- news/urls.py
+++ news/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from news.views import index, details_page
+from news.views import index, details_page, add_categories
 
 
 urlpatterns = [
     path("", index, name="home-page"),
-    path('/<int:id>', details_page, name="news-details-page")
+    path('/<int:id>', details_page, name="news-details-page"),
+    path('/categories', add_categories, name="categories-form")
 ]
