7f0b6d53dc233561801d7d5e33b1272b5769521f
Lívio Lopes <antonio.lvio760@gmail.com>
2023-12-08 10:56:37 -0300

    feat(urls.py): add route to register news

--- news/urls.py
+++ news/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from news.views import index, details_page, add_categories
+from news.views import index, details_page, add_categories, add_news
 
 
 urlpatterns = [
     path("", index, name="home-page"),
-    path('/<int:id>', details_page, name="news-details-page"),
-    path('/categories', add_categories, name="categories-form")
+    path("<int:id>/", details_page, name="news-details-page"),
+    path("categories/", add_categories, name="categories-form"),
+    path("news/", add_news, name="news-form"),
 ]
